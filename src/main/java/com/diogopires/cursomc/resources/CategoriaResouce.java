db326fbd888ba85388f9aaa740dd1892746e5079
Diogo Pires <dpirespereira86@gmail.com>
2021-11-04 16:43:07 -0300

    Methodo post categoria

--- src/main/java/com/diogopires/cursomc/resources/CategoriaResouce.java
+++ src/main/java/com/diogopires/cursomc/resources/CategoriaResouce.java
@@ -1,14 +1,17 @@
 package com.diogopires.cursomc.resources;
 
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.diogopires.cursomc.domain.Categoria;
 import com.diogopires.cursomc.services.CategoriaService;
@@ -25,5 +28,11 @@ public class CategoriaResouce {
 		Optional<Categoria> obj = Optional.ofNullable(service.find(id));
 		return ResponseEntity.ok().body(obj);
 	}
-
+    
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
