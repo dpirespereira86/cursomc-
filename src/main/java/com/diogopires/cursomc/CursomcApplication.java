689149ad3ae23a0f2237c30d4cdcc34561f9e776
Diogo Pires <dpirespereira86@gmail.com>
2021-11-04 12:19:43 -0300

    Pedido,EstadoPagamento e PAGAMENTO

--- src/main/java/com/diogopires/cursomc/CursomcApplication.java
+++ src/main/java/com/diogopires/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.diogopires.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.diogopires.cursomc.domain.Cidade;
 import com.diogopires.cursomc.domain.Cliente;
 import com.diogopires.cursomc.domain.Endereco;
 import com.diogopires.cursomc.domain.Estado;
+import com.diogopires.cursomc.domain.Pagamento;
+import com.diogopires.cursomc.domain.PagamentoComBoleto;
+import com.diogopires.cursomc.domain.PagamentoComCartao;
+import com.diogopires.cursomc.domain.Pedido;
 import com.diogopires.cursomc.domain.Produto;
+import com.diogopires.cursomc.domain.enums.EstadoPagamento;
 import com.diogopires.cursomc.domain.enums.TipoCliente;
 import com.diogopires.cursomc.repositories.CategoriaRepository;
 import com.diogopires.cursomc.repositories.CidadeRepository;
 import com.diogopires.cursomc.repositories.ClienteRepository;
 import com.diogopires.cursomc.repositories.EnderecoRepository;
 import com.diogopires.cursomc.repositories.EstadoRepository;
+import com.diogopires.cursomc.repositories.PagamentoRepository;
+import com.diogopires.cursomc.repositories.PedidoRepository;
 import com.diogopires.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienterepository;
 	@Autowired
 	private EnderecoRepository enderecorepository;
+	@Autowired
+	private PedidoRepository pedidorepository;
+	@Autowired
+	private PagamentoRepository pagamentorepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -85,6 +97,23 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienterepository.saveAll(Arrays.asList(cli1));
 		enderecorepository.saveAll(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido pedi1 = new Pedido(null, sdf.parse("30/09/2017 10:32"),cli1,e1 );
+		Pedido pedi2 = new Pedido(null, sdf.parse("10/10/2017 19:35"),cli1,e2 );
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, pedi1, 6);
+		pedi1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, pedi2, sdf.parse("20/10/2017 00:00"), null);
+		pedi2.setPagamento(pagto2);
+		
+		cli1.getPedido().addAll(Arrays.asList(pedi1,pedi2));
+		
+		pedidorepository.saveAll(Arrays.asList(pedi1,pedi2));
+		pagamentorepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
 		
 	}
 
--- src/main/java/com/diogopires/cursomc/domain/Cliente.java
+++ src/main/java/com/diogopires/cursomc/domain/Cliente.java
@@ -37,6 +37,9 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
     private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedido = new ArrayList<>();
     
     public Cliente() {
     	
@@ -106,6 +109,14 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(List<Pedido> pedido) {
+		this.pedido = pedido;
+	}
 
 	@Override
 	public int hashCode() {
@@ -123,6 +134,8 @@ public class Cliente implements Serializable {
 		Cliente other = (Cliente) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
 
 	
     
